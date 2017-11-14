871f42b9d73a64e6edc6d56a1192dab6b00917a8
S. Andrew Sheppard <andrew@wq.io>
2017-11-14 10:18:53 -0600

    update version for dev

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def parse_markdown_readme():
 
 setup(
     name='requirejs',
-    version='0.1.0',
+    version='0.1.1-dev',
     author='S. Andrew Sheppard',
     author_email='andrew@wq.io',
     url='https://github.com/wq/python-requirejs',
